f0c0c8ac80ec9253cdccd69bf8f743b7a90ba233
Kevin Chen <ningchen227@gmail.com>
2019-09-04 20:40:32 -0500

    Initial commit

---
+++ proj_1.c
@@ -0,0 +1,18 @@
+/**
+ * Project 1
+ * Name: Kevin Chen
+ * NetID: nkc160130
+ * Coded in: C
+ * Due: 09/28/19
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    
+}
